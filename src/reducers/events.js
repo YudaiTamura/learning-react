3c7f77b9b2f07b503abf418e9e85962b006cbf16
YudaiTamura <y-tam@live.jp>
2020-06-04 00:33:54 +0900

    eventの新規作成機能を追加

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,13 @@
 import _ from "lodash";
-import { READ_EVENTS } from "../actions";
+import { READ_EVENTS, CREATE_EVENT } from "../actions";
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, "id");
+    case CREATE_EVENT:
+      const data = action.response.data;
+      return { ...events, [data.id]: data };
     default:
       return events;
   }
